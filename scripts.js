12019d8062cb9eecae29396f80967827cafc308d
Md Imran Hosen <62979868+MdImranHosen1@users.noreply.github.com>
2024-02-19 03:00:14 +0600

    "More Project dynamic button added"

--- scripts.js
+++ scripts.js
@@ -13,3 +13,28 @@ function toggleVisibilityContactMessage() {
    
     }
 }
+
+
+function toggleVisibilityOtherProject()
+{
+    let other_project=document.getElementById("other-project-main-div")
+    
+    let see_more_project_button_img=document.getElementById("other-project-seemore-img");
+    let see_more_project_button_text=document.getElementById("other-project-seemore-text")
+
+    if(other_project.style.display==='none' || other_project.style.display === '')
+    {
+        other_project.style.display='block';
+        see_more_project_button_text.textContent="Collapse ";
+        
+        see_more_project_button_img.src="./images/viewless.png";
+    }
+    else{
+        other_project.style.display='none';
+        see_more_project_button_text.textContent="See More ";
+        see_more_project_button_img.src="./images/viewmore.png";
+        
+    }
+
+
+}
