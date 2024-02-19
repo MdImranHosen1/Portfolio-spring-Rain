0a38b84f4fc5a5ea2e20b661a2e4b6249b2e31ea
Md Imran Hosen <62979868+MdImranHosen1@users.noreply.github.com>
2024-02-19 15:38:42 +0600

    "Make navbar responsive. Large and small display version added in navbar"

--- scripts.js
+++ scripts.js
@@ -27,14 +27,75 @@ function toggleVisibilityOtherProject()
         other_project.style.display='block';
         see_more_project_button_text.textContent="Collapse ";
         
-        see_more_project_button_img.src="./images/viewLess.png";
+        see_more_project_button_img.src="./images/viewless.png";
     }
     else{
         other_project.style.display='none';
         see_more_project_button_text.textContent="See More ";
-        see_more_project_button_img.src="./images/viewMore.png";
+        see_more_project_button_img.src="./images/viewmore.png";
         
     }
+}
+
+function toggleVisibilityNavbar() {
+    let navbar_items = document.getElementsByClassName("flex-container");
+    let nevbar_menu_img=document.getElementById("navbar-2nd-div-2nd-div-img");
+
+    for (let i = 0; i < navbar_items.length; i++) {
+        let navbaritem = navbar_items[i];
+        navbaritem.classList.toggle("hidden");     
+    }
+    
+    let navbar_item = document.getElementsByClassName("flex-container")[0];
+    if(navbar_item.classList.contains('hidden'))
+    {
+        nevbar_menu_img.src="./images/list.png";
+    }
+    else{
+        nevbar_menu_img.src="./images/next.png";   
+    }
+
+}
+
+function handleResize() {
+    var width = window.innerWidth;
+
+    console.log(width);
+    if(width < 700)
+    {
+        let navbarlarge = document.getElementById('flex-container-head-large');
+        let navbarsmall = document.getElementById('flex-container-head-small');
+        navbarlarge.style.display='none'
+        navbarsmall.style.display='flex'
+
+    }
+    else
+    {
+        let navbarlarge = document.getElementById('flex-container-head-large');
+        let navbarsmall = document.getElementById('flex-container-head-small');
+        navbarlarge.style.display='flex'
+        navbarsmall.style.display='none'
+    }
+}
+
+document.addEventListener('DOMContentLoaded', handleResize);
+
+window.addEventListener('resize', handleResize);
 
 
+function handleClickSmallNavbarToggle()
+{
+    let navbar_item = document.getElementsByClassName("unorder-list-small-navbar")[0];
+    
+    
+    navbar_item.classList.toggle("hidden");     
+    let navbar_menu_img = document.getElementById("small-navbar-menu-img");
+    if(navbar_item.classList.contains('hidden'))
+    {
+        
+        navbar_menu_img.src="./images/list.png";   
+    }
+    else{
+        navbar_menu_img.src="./images/xmark.png";
+    }
 }
